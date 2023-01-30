f6187b4f1dfbc023c48920176acc862a9b847642
Elis Nilsson <elinil12@elevad.umea.se>
2023-01-30 10:36:05 +0100

    added clicker route

---
+++ src/routes/clicker/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let clicks = 0;
+    function clicked() {
+        clicks+= 1;
+        console.log(clicks);
+    }
+</script>
+
+
+
+<main>
+    <h1>clicker</h1>
+
+
+
+    <button on:click={clicked}>
+        click me {clicks}
+    </button>
+    du har clicka såhär mycke: {clicks}
+</main>
